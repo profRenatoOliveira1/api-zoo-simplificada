a7b685b2e25bc4f18ede4d3f8659bc78a3414bbd
Renato Oliveira <renato.loliveira89@gmail.com>
2024-04-24 10:13:49 -0300

    Criando método para remover uma ave do banco de dados

--- src/app.ts
+++ src/app.ts
@@ -119,6 +119,18 @@ server.post('/novo/atracao', async (req, res) => {
     }
 });
 
+server.delete('/remover/animal', async (req, res) => {
+    const idAnimal = parseInt(req.query.idAnimal as string);
+
+    const resultado = await Ave.removerAve(idAnimal);
+
+    if(resultado) {
+        return res.status(200).json('Animal foi removido com sucesso');
+    } else {
+        return res.status(401).json('Erro ao remover animal');
+    }
+});
+
 new DatabaseModel().testeConexao().then((resbd) => {
     if(resbd) {
         server.listen(port, () => {
